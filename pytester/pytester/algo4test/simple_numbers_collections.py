c9e89a351e2d966810d15928b4c9c6122e048c2e
Ivan Vekhov <Ivan.Vekhov@mediacom.com>
2020-02-06 09:15:44 +0300

    added eratosfen for n solution

--- pytester/pytester/algo4test/simple_numbers_collections.py
+++ pytester/pytester/algo4test/simple_numbers_collections.py
@@ -84,13 +84,26 @@ def find_simple_eratosfen_loglogn(nmax):
 
 
 # 5
+def find_simple_eratosfen_n(nmax):
+    arr = range(0, nmax + 1)
+    lp = [0 for _ in range(0, nmax + 1)]
+    pr = []
+
+    for i in (range(2, len(arr))):
+        if lp[i] == 0:
+            lp[i] = i
+            pr.append(i)
+        for p in pr:
+            if p <= lp[i] and p * i <= nmax:
+                lp[p * i] = p
+    return len(pr)
+
+
+# 6
 def find_simple_eratosfen_memopt(nmax):
     pass
 
 
-# 6
-def find_simple_eratosfen_n(nmax):
-    pass
 
 
 def main():
@@ -98,9 +111,9 @@ def main():
     # algo = find_simple_sqrt_array
     # algo = find_simple_nlogn
     # algo = find_simple_eratosfen_memopt
-    # algo = find_simple_eratosfen_n
+    algo = find_simple_eratosfen_n
+    # algo = find_simple_eratosfen_loglogn
 
-    algo = find_simple_eratosfen_loglogn
     nmax = 30
     print(algo(nmax))
 
@@ -111,21 +124,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-############################################################
-## def solution(first_file, second_file, algo):
-#     return algo.__name__, compare_files_simple(first_file, second_file, algo)
-    # a = '2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67
-    # 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499 503 509 521 523 541 547 557 563 569 571 577 587 593 599 601 607 613 617 619 631 641 643 647 653 659 661 673 677 683 691 701 709 719 727 733 739 743 751 757 761 769 773 787 797 809 811 821 823 827 829 839 853 857 859 863 877 881 883 887 907 911 919 929 937 941 947 953 967 971 977 983 991 997'
-    # q = a.split(' ')
-    # print(len(q))
-    # print(q[167])
-    # pass
-
-    # q = [42, 1, 99, 111, 4, -1]
-    # b = [True, False, True, False, True]
-    # for i in range(0, len(q)-1):
-    #     if b[i] is True:
-    #         print(q[i])
