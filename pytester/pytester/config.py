611b37d25b36b9b60ff9855da8c88149e71c9397
ivekhov <ivekhov@users.noreply.github.com>
2020-02-09 01:50:43 +0300

    updated config for chess tasks

--- pytester/pytester/config.py
+++ pytester/pytester/config.py
@@ -3,21 +3,22 @@ import os
 # from algo4test import gcd
 # from algo4test import simple_numbers
 # from algo4test import pow
-from algo4test import fib
+# from algo4test import fib
+from algo4test import chess
 
 # TASK = strings.solution
 # TASK = gcd.solution
-TASK = fib.solution
+# TASK = fib.solution
 # TASK = simple_numbers.solution
 # TASK = pow.solution
-
+TASK = chess.solution
 
 # DATA_FOLDER  = 'strings'
 # DATA_FOLDER = 'gcd'
 # DATA_FOLDER = 'degree'
-DATA_FOLDER = 'fib'
+# DATA_FOLDER = 'fib'
 # DATA_FOLDER = 'simple_numbers'
 # DATA_FOLDER = 'pow'
-
+DATA_FOLDER = os.path.join('chess', '1_king')  # '2_horse'  '3_fen'  '4_long'
 
 LOCAL_PATH = os.path.join('.', 'data4test', DATA_FOLDER)
