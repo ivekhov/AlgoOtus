641db77d4e87c437dd10da721484439e6bde2492
ivekhov <ivekhov@gmail.com>
2020-02-09 14:55:52 +0300

    updated chess.solution imports

--- pytester/pytester/algo4test/chess.py
+++ pytester/pytester/algo4test/chess.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 from .common import compare_files_chess
 # from .chess_collections import find_steps_king as algo
 from .chess_collections import find_steps_horse as algo
 
-def solution(first_file, second_file):
-    return algo.__name__, compare_files_chess(first_file, second_file, algo)
-=======
-from .common import compare_files_chess
-from .chess_collections import find_steps_king as algo
-
 
 def solution(first_file, second_file):
     return algo.__name__, compare_files_chess(first_file, second_file, algo)
->>>>>>> origin/master
