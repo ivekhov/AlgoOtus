a87fe7a1b177914284512c2f0bc1a5eb8477f573
Ivan.Vekhov <Iv@n)27011985>
2020-02-23 01:11:16 +0300

    added files of MergeSort

--- Sorting/src/Program.java
+++ Sorting/src/Program.java
@@ -4,11 +4,36 @@ import BubbleSort.BubblesSort;
 import InsertionSort.InsertionSort;
 import SelectSort.SelectSort;
 import ShellSort.ShellSort;
+import QuickSort.QuickSort;
+import MergeSort.MergeSort;
 
 public class Program {
     public static void main(String[] args)
     {
         int[] arr = {5, 7 ,2, 4 ,3, 9, 1, 0, 6, 8, 42, 17, 99, -2, -999, 100012};
+
+
+        int[] A = {5, 42, 111, 245, 521, 999, 10500, 0, -31, 13};
+        MergeSort myMergeSort = new MergeSort(A);
+        myMergeSort.Sort(0, A.length - 1);
+        myMergeSort.Print();
+
+
+//        MergeSort myMergeSort = new MergeSort();
+//        int[] A = {5, 42, 111, 245, 521, 999, 10500};
+//        int[] B = {1, 7, 99, 187, 198, 666, 5645};
+//        int [] S = myMergeSort.merge(A, B);
+//        for (int i = 0; i < S.length; i++) {
+//            System.out.print(S[i] + " ");
+//        }
+
+
+//        QuickSort myQsort = new QuickSort(arr);
+//        int a = myQsort.partition(6);
+//        myQsort.Print();
+//        myQsort.Sort(0, arr.length - 1);
+//        myQsort.Print();
+
 
 
 //        ShellSort myShellSort = new ShellSort(arr);
@@ -37,10 +62,10 @@ public class Program {
 //        HeapSort myHeapSort = new HeapSort(arr);
 //        myHeapSort.HeapSort();
 
-        HeapSortRepeat myHeapSortRep = new HeapSortRepeat(arr);
-        myHeapSortRep.Print();
-        myHeapSortRep.Sort();
-        myHeapSortRep.Print();
+//        HeapSortRepeat myHeapSortRep = new HeapSortRepeat(arr);
+//        myHeapSortRep.Print();
+//        myHeapSortRep.Sort();
+//        myHeapSortRep.Print();
 
     }
 }
