ffd2912f29ee37f1e7abb645af35af5cf8f31380
Ivan Vekhov <ivekhov@gmail.com>
2020-02-26 00:56:23 +0300

    added local sort files

---
+++ Sorting/src/HeapSort/Program.java
@@ -0,0 +1,20 @@
+package HeapSort;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+    public static void HeapSort(int[] array){
+
+
+    }
+    void Down(int size, int root){
+        //
+    }
+    void Swap(int a, int b){
+        //
+    }
+    Down(arr.length, 0);
+    Swap(0, arr.length - 1);
+    Down(arr.length - 1);
+}
